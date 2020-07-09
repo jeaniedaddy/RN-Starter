ead8a5672084407eb80bfc8d9b18bf17b0c56d84
No <jeaniedaddy@gmail.com>
2020-07-09 09:12:24 -0400

    initial commit

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View, StyleSheet } from "react-native";
+
+
+const  ComponentsScreen = () => {
+    return (
+        <View>
+            <Text>Components Screen</Text>
+        </View>
+
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default ComponentsScreen;
---
+++ src/screens/ListScreen.js
@@ -0,0 +1,42 @@
+
+import React from 'react';
+import {Text, View, StyleSheet, FlatList} from "react-native";
+
+
+const  ListScreen = () => {
+    const friends = [
+        {name: 'friends #1', age: 24},
+        {name: 'friends #2', age: 14},
+        {name: 'friends #3', age: 34},
+        {name: 'friends #4', age: 32},
+        {name: 'friends #5', age: 4},
+        {name: 'friends #6', age: 94},
+        {name: 'friends #7', age: 11},
+        {name: 'friends #8', age: 35},
+        {name: 'friends #9', age: 49}
+    ];
+    return (
+        //<Text style={styles.text}>List Screen</Text>
+        <FlatList 
+            //horizontal
+            showsHorizontalScrollIndicator={false}
+            showsVerticalScrollIndicator
+            keyExtractor={(item)=>{ return item.name}}
+            data={friends} 
+            renderItem={({item, index})=>{
+                return (
+                    <Text style={styles.textStyle}>{item.name + '- Age: ' + item.age}</Text>
+                );
+            }} 
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 30,
+        marginVertical: 40
+    }
+});
+
+export default ListScreen;
